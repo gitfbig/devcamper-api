ebb9b0f22286cd7f0c8e45c8d729c28283186db3
steven <github-deskop@gmail.com>
2021-01-28 21:21:39 +0700

    Initial bootcamp routes and controller

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const bootcamps = require('./routes/bootcamps')
 
 const app = express()
 dotenv.config({ path: './config/config.env' })
@@ -8,6 +10,14 @@ const port = process.env.PORT || 5000
 const server = app.listen(port, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
 })
+
+// app's middlewares
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 // todo:
 if (require.main !== module) {
